492c973a678b8ac5922fd507dfbe600e7b6fa7dd
kazuki06kawamitu <138412466+kazuki06kawamitu@users.noreply.github.com>
2023-10-04 14:42:40 +0900

    テトリス
    BLOCK 定数定義から

--- Machi3/Tetris/Block_cpp.cpp
+++ Machi3/Tetris/Block_cpp.cpp
@@ -13,3 +13,36 @@
 #define BLOCK_NEXT_POS_X (700) //次のブロックの座標(x座標)
 #define BLOCK_NEXT_POS_Y (500) //次のブロックの座標(y座標)
 #define BLOCK_STOCK_POS_X (500)//ストックされたブロックの座標(x座標)
+#define BLOCK_STOCK_POS_Y (350)//ストックされたブロックの座標(ｙ座標)
+#define DROP_BLOCK_INIT_X (4)  //落ちてくるブロックの初期ｘ座標
+#define DROP_BLOCK_INIT_Y (-1) //落ちてくるブロックの初期ｙ座標
+#define DROP_SPEED  (60)       //落下時間
+#define TURN_CROCKWICE (0)     //時計回りに回転させる
+#define TURN_ANTICROKWICE (1)  //反時計回りに回転させる
+
+/****************************************
+*型定義
+****************************************/
+enum BLOCK_STATE
+{
+	E_BLOCK_EMPTY,             //空ブロック
+	E_BLOCK_LIGHT_BLUE,        //水色
+	E_BLOCK_YELLOW_GREEN,      //黄緑
+	E_BLOCK_YELLOW,            //黄色
+	E_BLOCK_ORANGE,            //オレンジ
+	E_BLOCK_BLUE,              //青
+	E_BLOCK_PINK,              //ピンク
+	E_BLOCK_RED,               //赤
+	E_BLOCK_GLAY,              //灰色
+	E_BLOCK_WALL,              //壁
+	E_BLOCK_IMAGE_MAX,
+};
+
+/******************************************
+*定数定義
+*******************************************/
+const int C_BLOCK_TABLE[BLOCK_TYPE_MAX][BLOCK_TROUT_SIZE][BLOCK_TROUT_SIZE] = {
+	{
+
+}
+};
